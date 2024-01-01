268625b3cc8065571d22dafcea578d641884415d
Nikos Saridis <ncsaridi@gmail.com>
2024-01-01 21:23:28 +0200

    Initialize Repo

---
+++ example_script.R
@@ -0,0 +1,21 @@
+# =============================================================================
+# Example R Script
+# =============================================================================
+
+# Load packages
+library(dplyr)
+
+# Load data
+data("mtcars")
+
+# I'm not sure what's in the mtcars data. I'm printing it below to take a look
+mtcars
+
+## Data analysis
+# ----------------------------------------------------------------------------
+
+# Below, we calculate the average mpg across all cars in the mtcars data frame.
+mean(mtcars$mpg)
+
+# Here, we also plot mpg against displacement.
+plot(mtcars$mpg, mtcars$disp)
